152ab4d919d2ab6531ef0f255220659024eae53a
Jonathon Langel <jonathon.langel@zaneray.com>
2020-10-20 17:08:18 -0500

    add better validation, remove unused imports

--- routes/email.js
+++ routes/email.js
@@ -1,29 +1,35 @@
 "use strict";
 
-const get = require("lodash/get");
-const EmailService = require("../services/email");
 const express = require("express");
 const router = express.Router();
 const createError = require('http-errors');
-const invariant = require('invariant');
+
+const Joi = require('Joi');
+const EmailService = require("../services/email");
+
+const uniqueEmailInputSchema = Joi.object().keys({
+    emails: Joi.array().items(
+        Joi.string().email()
+    )
+})
 
 router.post("/api/emails/unique", async (req, res, next) => {
-  try {
     try {
-        // since it's a simple service, we'll use invariant, normally Joi or express-validator would be my first options
-        invariant(!!req.body.emails, 'emails array is required for this operation');
-    } catch (error) {
-      throw new createError(400, error);
-    }
+        let value;
+        try {
+            value = await uniqueEmailInputSchema.validateAsync(req.body);
+        } catch (error) {
+            throw new createError(400, error);
+        }
 
-    const uniqueEmailResponse = {
-        uniqueEmailAddressCount: EmailService.filterUniqueEmails(req.body.emails)
+        const uniqueEmailResponse = {
+            uniqueEmailAddressCount: EmailService.filterUniqueEmails(value.emails)
+        }
+
+        res.json(uniqueEmailResponse);
+    } catch (error) {
+        next(error);
     }
-    
-    res.json(uniqueEmailResponse);
-  } catch (error) {
-    next(error);
-  }
 });
 
 module.exports = router;
